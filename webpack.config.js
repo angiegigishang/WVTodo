4fd723af40f578f751c68c5d4fe04f842d52a73f
shangxy <shangxy@zjrealtech.com>
2019-03-14 18:05:15 +0800

    error

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports = {
-	mode: 'production',
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+	//mode: 'production',
+	target: 'web',
 	entry: path.join(__dirname, 'src/index.js'),
 	output: {
 		filename: 'bundle.js',
 		path: path.join(__dirname, 'dist')
 	},
 	plugins: [
-		new VueLoaderPlugin()
+		new webpack.definePlugin({
+			'process.env': {
+				NODE_ENV: isDev ? '"development"' : '"production"'
+			}
+		}),
+		new VueLoaderPlugin(),
+		new HTMLPlugin()
 	],
 	module: {
 		rules: [{
@@ -37,3 +48,15 @@ module.exports = {
 		}]
 	}
 } 
+
+if (isDev) {
+	config.devServer = {
+		port: 8000,
+		host: '0.0.0.0',
+		overlay: {
+			errors: true
+		}
+	}
+}
+
+module.exports = config
