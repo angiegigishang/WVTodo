f102ef5770d78de58e7a01851d2b716aead4e57c
shangxy <shangxy@zjrealtech.com>
2019-03-14 17:03:06 +0800

    webpack配置2

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,23 @@ module.exports = {
 			loader: 'vue-loader'
 		}, {
 			test: /\.css$/,
-			use: ['css-loader']
+			use: ['style-loader','css-loader']
+		}, {
+			test: /\.(gif|jpg|jpeg|png|svg)$/,
+			use: [{
+				loader: 'url-loader',
+				options: {
+					limit: 1024,
+					name: '[name]-aaa.[ext]'
+				}
+			}]
+		}, {
+			test: /\.styl$/,
+			use: [
+				'style-loader',
+				'css-loader',
+				'stylus-loader'
+			]
 		}]
 	}
 } 
